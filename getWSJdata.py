2073cd75e4273b547e1854c3dae0ac263bd25888
Daniel <danielrferriss@gmail.com>
2018-04-05 21:33:46 -0500

    crawls WSJ page on any stock

--- getWSJdata.py
+++ getWSJdata.py
@@ -35,8 +35,52 @@ def get_1day_range(page_soup):
 def get_52wk_range(page_soup):
 	return page_soup.find(class_='cr_data_collection cr_charts_info').find_all(class_='data_data')[3].get_text()
 
+def get_Qestimate_current(page_soup):
+	return page_soup.find(id='quarterlyEstimateTbl').find_all(class_='valueCell')[0].get_text()
+
+def get_Qestimate_1mnth(page_soup):
+	return page_soup.find(id='quarterlyEstimateTbl').find_all(class_='valueCell')[1].get_text()
+
+def get_Qestimate_3mnth(page_soup):
+	return page_soup.find(id='quarterlyEstimateTbl').find_all(class_='valueCell')[2].get_text()
+
+def get_nxtQestimate_current(page_soup):
+	return page_soup.find(class_='cr_dataTable cr_dataTable-plain crTable-trends').find_all(class_='valueCell')[0].get_text()
+
+def get_nxtQestimate_1mnth(page_soup):
+	return page_soup.find(class_='cr_dataTable cr_dataTable-plain crTable-trends').find_all(class_='valueCell')[1].get_text()
+
+def get_nxtQestimate_3mnth(page_soup):
+	return page_soup.find(class_='cr_dataTable cr_dataTable-plain crTable-trends').find_all(class_='valueCell')[2].get_text()
+
+def get_Qincome_growth(page_soup):
+	return page_soup.find(class_='cr_financials_table').find(class_='data_data').get_text()
+
+def get_Qsales(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[1].get_text()
+
+def get_Qsales_growth(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[2].get_text()
+
+def get_QEBITDA(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[3].get_text()
+
+def get_Aincome_growth(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[4].get_text()
+
+def get_Asales(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[5].get_text()
+
+def get_Asales_growth(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[6].get_text()
+
+def get_AEBITDA(page_soup):
+	return page_soup.find(class_='cr_financials_table').find_all(class_='data_data')[7].get_text()
+
+
+
 
 
 tsla = get_wsj_data('TSLA')
 
-print(get_open(tsla))
+print(get_closing(tsla))
